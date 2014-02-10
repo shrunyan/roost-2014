a1655f551a25af6f4da199a5f7037b02cbe9537c
Stuart Runyan <shrunyan@gmail.com>
2014-02-10 11:33:19 -0800

    Basic grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,69 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		clean: {},
-		concat: {},
-		copy: {},
-		jshint: {},
-		watch: {},
+		clean: {
+			prod: 'prod',
+		},
+		connect: {
+			dev: {
+				options: {
+					base: ['prod'],
+					port: 8000,
+					hostname: '*'
+				}
+			}
+		},
+		copy: {
+			app: {
+				src: 'src/pages/index.html',
+				dest: 'prod/index.html'
+			}
+		},
+		jshint: {
+			build: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: ['Gruntfile.js']
+			},
+			app: {
+				options: {
+					jshintrc: 'src/.jshintrc'
+				},
+				src: ['src/**/*.js']
+			}
+		},
+		watch: {
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: ['src/**/*.{js,css}', 'prod/*'],
+				tasks: [],
+			},
+			page: {
+				files: ['src/**/*.html'],
+				tasks: ['livereload']
+			},
+			jshintrc: {
+				files: ['**/.jshintrc'],
+				tasks: ['jshint']
+			},
+			script: {
+				files: ['<%= jshint.app.src %>'],
+				tasks: ['jshint:app']
+			}
+		},
 	});
 
-	gurnt.registerTask('default', []);
+	grunt.registerTask('default', ['dev']);
+	grunt.registerTask('dev', ['jshint', 'clean:prod', 'copy:app', 'connect:dev', 'watch']);
 
 
-	grunt.loadNpmTasks('grunt-contirb-clean');
-	grunt.loadNpmTasks('grunt-contirb-concat');
-	grunt.loadNpmTasks('grunt-contirb-copy');
-	grunt.loadNpmTasks('grunt-contirb-jshint');
-	grunt.loadNpmTasks('grunt-contirb-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 }
